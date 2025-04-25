fb529f3294a7b8ccd5f6fbdfea9f62386300b7a3
cursosbackend <cursosbackends@gmail.com>
2025-04-25 01:49:31 +0000

    codigo de test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
-console.log('jQuery versión:', $.fn.jquery);
-console.log('Bootstrap Modal existe?', typeof bootstrap.Modal);
 
 
+
+
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+$('#btn').click(function(){
+    alert("correo enviado correctamente")
+
+})
+
+$('#btn2').dblclick(function(){
+    alert("boton clickeado 2 veces!!!!")
+
+})
+
+$('.hover').hover(
+    function() {$(this).css('background-color', 'yellow')},
+    function() {$(this).css('background-color', '')},
+)
+
+
+
+$('h2').dblclick(function(){
+    $(this).css('color', 'red')
+})
+
+
+$('#btntoggle').click(function(){
+    $('#contenido').toggle()
+})
 
 
